38143c1fd2f14439ec52c5977e2679d734d348c3
Lkhagvasuren <lkhagvaasuren12@gmail.com>
2022-11-07 23:23:21 +0800

    oorchlov

--- python.py
+++ python.py
@@ -1,7 +1,18 @@
-a = int(input("a: "))
+#a toonoos b toonii niilber oloh
+a=int(input("a too: "))
 b = int(input("b: "))
 
 niilber = 0
 for i in range(1, b+1):
     niilber = niilber + a
 print(niilber)
+
+#n toonii tsipriin niilberiig oloh
+n=input('too oruul ')
+urt=len(n)
+niilber=0
+for i in range(urt):
+     niilber=niilber+int(n[i])
+print(niilber)
+
+#
